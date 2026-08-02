0a4fa91e423c69eee58a4bbbcb8f55b1945f816e
james-tindal <james@jamestindal.co.uk>
2026-08-02 18:43:22 +0200

    add type assertion utilities

--- src/utilities.ts
+++ src/utilities.ts
@@ -2,3 +2,18 @@ export function assertExists<T>(x: T, message?: string): asserts x is NonNullabl
   if (x === null || x === undefined)
     throw new TypeError(message ?? 'Value must not be null or undefined')
 }
+
+type TupleOf<T, N extends number, R extends T[] = []> =
+  R['length'] extends N ? R : TupleOf<T, N, [...R, T]>
+
+export type ArrayWithLength<T, N extends number> =
+  number extends N ? T[] : TupleOf<T, N>
+
+export function assertLength<T, N extends number>(
+  array: T[],
+  length: N,
+  message?: string,
+): asserts array is ArrayWithLength<T, N> {
+  if (array.length !== length)
+    throw new RangeError(message ?? `Array must contain exactly ${length} item(s)`)
+}
