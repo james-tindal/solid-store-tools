a41d52686824c076319187c5d4ea3b1d15daa0c2
james-tindal <james@jamestindal.co.uk>
2026-09-16 00:21:30 +0200

    initial commit

---
+++ eslint.config.ts
@@ -0,0 +1,50 @@
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+import { Linter } from 'eslint'
+
+export default <Linter.Config[]>[{
+  files: ['**/*.ts'],
+  ignores: ['node_modules/'],
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  plugins: {
+    '@typescript-eslint': tseslint.plugin,
+    '@stylistic': stylistic,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', {
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_',
+    }],
+    // '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    'prefer-const': 'error',
+    '@stylistic/arrow-parens': ['error', 'as-needed'],
+    '@stylistic/comma-spacing': 'error',
+    '@stylistic/object-curly-spacing': ['error', 'always', {
+      objectsInObjects: false,
+      emptyObjects: 'never',
+    }],
+    '@stylistic/space-in-parens': ['error', 'never'],
+    '@stylistic/array-bracket-spacing': ['error', 'never', {
+      objectsInArrays: false
+    }],
+    '@stylistic/quotes': ['error', 'single', {
+      avoidEscape: true
+    }],
+    '@stylistic/semi': ['error', 'never', {
+      beforeStatementContinuationChars: 'never'
+    }],
+    '@stylistic/semi-style': ['error', 'first'],
+    '@stylistic/generator-star-spacing': ['error', 'after'],
+    '@stylistic/arrow-spacing': ['error', { before: true, after: true }],
+    '@stylistic/computed-property-spacing': 'error',
+    '@stylistic/block-spacing': 'error',
+    '@stylistic/padded-blocks': ['error', 'never'],
+    '@stylistic/space-before-blocks': 'error',
+  },
+}]
